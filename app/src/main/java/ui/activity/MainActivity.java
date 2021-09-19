7c24c8ea262fa7b1662668bacacba92ff77eddaa
hamidreza_mj <hamid.mj313@gmail.com>
2021-09-19 14:05:22 +0430

    improve toast ui, ViewBinding added in MainActivity

--- app/src/main/java/ui/activity/MainActivity.java
+++ app/src/main/java/ui/activity/MainActivity.java
@@ -3,12 +3,22 @@ package ui.activity;
 import android.os.Bundle;
 
 import hlv.cute.todo.R;
+import hlv.cute.todo.databinding.ActivityMainBinding;
+import utils.ToastHelper;
 
 public class MainActivity extends BaseActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initViews();
+    }
+
+    private void initViews() {
+
     }
 }
