3e81481a019f51d6f12c29eba892190b624ce006
hamidreza_mj <hamid.mj313@gmail.com>
2021-09-24 17:10:14 +0330

    global interfaces removed

--- app/src/main/java/ui/adapter/event/OnCheckChangedListener.java
+++
@@ -1,7 +0,0 @@
-package ui.adapter.event;
-
-import model.Todo;
-
-public interface OnCheckChangedListener {
-    void onChange(Todo todo);
-}
--- app/src/main/java/ui/adapter/event/OnClickMenuListener.java
+++
@@ -1,7 +0,0 @@
-package ui.adapter.event;
-
-import model.Todo;
-
-public interface OnClickMenuListener {
-    void onClick(Object object);
-}
