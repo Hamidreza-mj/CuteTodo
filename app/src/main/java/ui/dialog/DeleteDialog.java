41ab05f814c1bd968b1497063b3702a0eff86a9b
hamidreza_mj <hamid.mj313@gmail.com>
2021-09-24 16:22:40 +0330

    DeleteDialog improved

--- app/src/main/java/ui/dialog/DeleteDialog.java
+++ app/src/main/java/ui/dialog/DeleteDialog.java
@@ -3,19 +3,42 @@ package ui.dialog;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.widget.TextView;
 
 import hlv.cute.todo.R;
+import hlv.cute.todo.databinding.DialogDeleteBinding;
 
 public class DeleteDialog {
 
     private AlertDialog dialog;
 
+    private final TextView txtTitle;
+    private final TextView txtMessage;
+
+    private OnClickDelete onClickDelete;
+
     public DeleteDialog(Context context, boolean cancelable) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.TranslucentDialog);
         builder.setCancelable(cancelable);
-        View view = LayoutInflater.from(context).inflate(R.layout.dialog_delete, null, false);
-        builder.setView(view);
+        hlv.cute.todo.databinding.DialogDeleteBinding binding = DialogDeleteBinding.inflate(LayoutInflater.from(context), null, false);
+        builder.setView(binding.getRoot());
+
+        txtTitle = binding.txtTitle;
+        txtMessage = binding.txtMessage;
+        TextView txtCancel = binding.txtCancel;
+        TextView txtDelete = binding.txtDelete;
+
+        txtCancel.setOnClickListener(view -> dismiss());
+        txtDelete.setOnClickListener(view -> {
+            if (onClickDelete == null) {
+                if (dialog != null)
+                    dismiss();
+
+                return;
+            }
+
+            onClickDelete.onClick();
+        });
 
         if (dialog == null)
             dialog = builder.create();
@@ -25,18 +48,36 @@ public class DeleteDialog {
         this(context, true);
     }
 
+    public void setOnClickDelete(OnClickDelete onClickDelete) {
+        this.onClickDelete = onClickDelete;
+    }
 
-    public DeleteDialog dismiss() {
+    public void dismiss() {
         if (dialog != null)
             dialog.dismiss();
 
-        return this;
     }
 
     public void show() {
         if (dialog != null)
             dialog.show();
 
+    }
+
+    public void setTitle(String title) {
+        if (dialog != null)
+            txtTitle.setText(title);
+
+    }
+
+    public void setMessage(String message) {
+        if (dialog != null)
+            txtMessage.setText(message);
+
+    }
+
+    public interface OnClickDelete {
+        void onClick();
     }
 
 }
