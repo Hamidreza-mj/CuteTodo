05b07b88a5bd5f704aebe7dbf70dfba6e354d74e
hamidreza_mj <hamid.mj313@gmail.com>
2022-07-16 12:21:48 +0430

    migrate TodoDatabase to kotlin

--- app/src/main/java/repo/database/TodoDatabase.kt
+++ app/src/main/java/repo/database/TodoDatabase.kt
@@ -1,47 +1,52 @@
-package repo.database;
+package repo.database
 
-import android.content.Context;
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import model.Category
+import model.Notification
+import model.Todo
+import repo.dao.CategoryDao
+import repo.dao.NotificationDao
+import repo.dao.TodoDao
 
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
+@Database(
+    entities = [Todo::class, Category::class, Notification::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TodoDatabase : RoomDatabase() {
 
-import model.Category;
-import model.Notification;
-import model.Todo;
-import repo.dao.CategoryDao;
-import repo.dao.NotificationDao;
-import repo.dao.TodoDao;
+    abstract val todoDao: TodoDao?
+    abstract val categoryDao: CategoryDao?
+    abstract val notificationDao: NotificationDao?
 
-@Database(entities = {Todo.class, Category.class, Notification.class}, version = 1, exportSchema = false)
-public abstract class TodoDatabase extends RoomDatabase {
+    companion object {
+        private const val DB_NAME = "TodoDB"
+        private var instance: TodoDatabase? = null
 
-    private static final String DB_NAME = "TodoDB";
-    private static TodoDatabase instance;
-
-    public abstract TodoDao getTodoDao();
-
-    public abstract CategoryDao getCategoryDao();
-
-    public abstract NotificationDao getNotificationDao();
-
-    public static synchronized TodoDatabase get(Context context) {
-        if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), TodoDatabase.class, DB_NAME)
+        @Synchronized
+        operator fun get(context: Context): TodoDatabase? {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TodoDatabase::class.java,
+                    DB_NAME
+                )
                     .fallbackToDestructiveMigration()
                     //.addCallback(roomCallBack)
                     //.allowMainThreadQueries() this allow room to run db process in main thread (UI Thread)
                     //it is recommended to be disallowed
-                    .build();
+                    .build()
+            }
+            return instance
         }
-        return instance;
+
+        /*private val roomCallBack: Callback = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+            }
+        }*/
     }
-
-    /*private static final RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-        }
-    };*/
-
 }
