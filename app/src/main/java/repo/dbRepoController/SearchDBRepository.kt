f0e3d457d408e0504d3e5c13c7fda9831ab28abd
hamidreza_mj <hamid.mj313@gmail.com>
2022-07-16 13:01:03 +0430

    migrate SearchDBRepository to kotlin

--- app/src/main/java/repo/dbRepoController/SearchDBRepository.kt
+++ app/src/main/java/repo/dbRepoController/SearchDBRepository.kt
@@ -1,49 +1,51 @@
-package repo.dbRepoController;
+package repo.dbRepoController
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import hlv.cute.todo.App
+import model.Todo
+import repo.dao.TodoDao
 
-import java.util.List;
+class SearchDBRepository {
 
-import hlv.cute.todo.App;
-import model.Todo;
-import repo.dao.TodoDao;
+    private val dao: TodoDao = App.get()!!.todoDatabase()!!.todoDao!!
+    private val _todosLive: MutableLiveData<List<Todo>?> = MutableLiveData()
+    val todosLive: LiveData<List<Todo>?> = _todosLive
 
-public class SearchDBRepository {
-
-    private final TodoDao dao;
-    private final MutableLiveData<List<Todo>> todosLive;
-
-    public SearchDBRepository() {
-        dao = App.get().todoDatabase().getTodoDao();
-        todosLive = new MutableLiveData<>();
+    fun initFetch() {
+        Thread {
+            _todosLive.postValue(dao.getAllTodos())
+        }.start()
     }
 
-    public void initFetch() {
-        new Thread(() -> todosLive.postValue(dao.getAllTodos())).start();
+    fun initFetch(categoryId: Int) {
+        Thread {
+            _todosLive.postValue(dao.getTodosWithCategory(categoryId))
+        }.start()
     }
 
-    public void initFetch(int categoryId) {
-        new Thread(() -> todosLive.postValue(dao.getTodosWithCategory(categoryId))).start();
+    fun searchTodo(term: String?) {
+        Thread {
+            _todosLive.postValue(dao.searchTodo(term))
+        }.start()
     }
 
-    public void searchTodo(String term) {
-        new Thread(() -> todosLive.postValue(dao.searchTodo(term))).start();
+    fun searchTodo(term: String?, categoryId: Int) {
+        Thread {
+            _todosLive.postValue(dao.searchTodoWithCategoryId(term, categoryId))
+        }.start()
     }
 
-    public void searchTodo(String term, int categoryId) {
-        new Thread(() -> todosLive.postValue(dao.searchTodoWithCategoryId(term, categoryId))).start();
+    fun searchCategory(term: String?) {
+        Thread {
+            _todosLive.postValue(dao.searchCategory(term))
+        }.start()
     }
 
-    public void searchCategory(String term) {
-        new Thread(() -> todosLive.postValue(dao.searchCategory(term))).start();
+    fun searchTodoWithCategory(term: String?) {
+        Thread {
+            _todosLive.postValue(dao.searchTodoWithCategory(term))
+        }.start()
     }
 
-    public void searchTodoWithCategory(String term) {
-        new Thread(() -> todosLive.postValue(dao.searchTodoWithCategory(term))).start();
-    }
-
-    public LiveData<List<Todo>> getTodosLive() {
-        return todosLive;
-    }
 }
